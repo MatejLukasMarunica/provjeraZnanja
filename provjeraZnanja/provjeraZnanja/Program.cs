d4bf6220af324b981d8188c756fe262d2b23d980
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-11-15 08:16:34 +0100

    Add files via upload

---
+++ provjeraZnanja/provjeraZnanja/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraZnanja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string recenica, novo;
+
+            Console.WriteLine(" Unesi neku rečenicu u string. ");
+            recenica = Console.ReadLine();
+
+            novo = recenica.Replace(" ", "_");
+
+            Console.WriteLine(novo);
+
+            Console.ReadKey();
+        }
+    }
+}
