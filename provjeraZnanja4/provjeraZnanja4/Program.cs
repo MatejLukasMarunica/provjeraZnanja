6f7c2bc3fd2d54752f8989e39315c1c4a642cb85
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-11-15 09:29:34 +0100

    Add files via upload

---
+++ provjeraZnanja4/provjeraZnanja4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraZnanja4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string brojevi;
+            string broj1, broj2, broj3;
+
+            Console.WriteLine(" Unesi 3 broja u niz odvojene razmakom. ");
+            brojevi = Console.ReadLine();
+
+            broj1 = brojevi.Substring(0, brojevi.IndexOf(" "));
+            broj3 = brojevi.Substring(brojevi.LastIndexOf(" ", brojevi.Length - 1));
+            broj2 = brojevi.Substring(brojevi.IndexOf(" "), brojevi.LastIndexOf(" "));
+
+            Convert.ToDouble(broj1);
+            Convert.ToDouble(broj2);
+            Convert.ToDouble(broj3);
+
+            
+
+        }
+    }
+}
